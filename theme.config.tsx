c1de67ee1a3756feb9b6a7bd18bd2c58aa6e9b60
Vincenzo Ursano <vin16u@gmail.com>
2023-07-16 19:26:17 +0200

    initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/vincenzoursano/prompt-equipment',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/vincenzoursano/prompt-equipment',
+  footer: {
+    text: 'Prompt Equipment',
+  },
+}
+
+export default config
