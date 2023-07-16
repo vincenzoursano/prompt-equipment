0112e0634c4e3b0e2843a85ab064c408af918ca1
Vincenzo Ursano <vin16u@gmail.com>
2023-07-16 20:08:39 +0200

    ci: deploy github

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  defaultShowCopyCode: true,
+  latex: true,
+  staticImage: true,
 })
 
-module.exports = withNextra()
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true
+  }
+}
+
+module.exports = withNextra(nextConfig)
+ 
